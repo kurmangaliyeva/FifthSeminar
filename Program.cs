a0b973e9219fdef14b7803a1a3f1acec08bf512b
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-08-11 14:11:37 +0600

    Second Task solved

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace FifthSeminar
             // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
             // 3, 5 -> 243 (3⁵)
             // 2, 4 -> 16
+
+            /*
             Console.WriteLine("Программа возводит число A в степень B");
 
             int Exponentiation(int numberA, int numberB){
@@ -26,8 +28,34 @@ namespace FifthSeminar
             int numberB = Convert.ToInt32(Console.ReadLine());
 
             int exponentiation = Exponentiation(numberA, numberB);
-            Console.WriteLine("Ответ: " + exponentiation);
+            Console.WriteLine("Ответ: " + exponentiation);  */
 
+
+            // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+            // 452 -> 11
+            // 82 -> 10
+            // 9012 -> 12
+
+            Console.WriteLine("Программа выдаёт сумму цифр в числе");
+            Console.Write("Введите число N ");
+            int numberN = Convert.ToInt32(Console.ReadLine());
+
+            int SumNumber(int numberN){
+                
+                int counter = Convert.ToString(numberN).Length;
+                int advance = 0;
+                int result = 0;
+
+                for (int i = 0; i < counter; i++){
+                advance = numberN - numberN % 10;
+                result = result + (numberN - advance);
+                numberN = numberN / 10;
+                }
+            return result;
+            }
+
+            int sumNumber = SumNumber(numberN);
+            Console.WriteLine("Сумма цифр в числе: " + sumNumber);
 
         }
     }
